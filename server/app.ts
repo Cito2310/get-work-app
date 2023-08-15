47c5f86d3d425efa607765da4b271b4f38b32047
Juan Meza <cito2310@gmail.com>
2023-08-15 19:18:50 -0300

    added script to get offers works in Bumeran

---
+++ server/app.ts
@@ -0,0 +1,135 @@
+import { writeFileSync } from "fs";
+import puppeteer from "puppeteer";
+import { JobOffer } from "./types/jobOffer";
+
+const day = 1;
+const typeJob = "programador";
+
+const baseUrl = "https://www.bumeran.com.ar";
+
+const createURL = ( page: number ) => `${baseUrl}/empleos-publicacion-menor-a-${day}-dias-busqueda-${typeJob}.html?page=${page}`;
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+    // page.on('console', msg => console.log(msg.text()));      // DEV - obtiene los console.log, ayuda al desarrollar
+    
+
+    let allWorks = [];
+    let worksFounds;
+    let currentPage = 0;
+
+    do {
+        worksFounds = [];   // inicializa un array para guardar todos los trabajo de la pagina actual
+        currentPage++;      // aumenta la pagina actual
+
+        await page.goto( createURL( currentPage ) );
+
+        await page.waitForSelector("#listado-avisos")           // espera a que la pagina y las ofertas de carguen
+
+        // VARIABLE - OBTIENE UN ARRAY CON UN OBJETO DE LA INFORMACION DE LAS OFERTAS
+        const offersWorksPage: JobOffer[] = await page.evaluate( () => {
+            const div = document.querySelector("#listado-avisos");  // esta es la seccion que contienete todos las ofertas de trabajo
+            const children = Array.from(div!.children);             // obtiene todos los hijos de la seccion anterior
+    
+            const sectionOffers = children
+                .filter(( element => element.tagName === "DIV" ))   // obtiene unicamente los divs, que son las ofertas y una parte inutil
+                .map( element => element.firstElementChild );       // obtiene el elemento que contiene toda la informacion de la oferta
+    
+            const onlyOffers = sectionOffers.slice (0, sectionOffers.length-1 ); // elimina la parte inutil que se encontraba al final del array
+    
+    
+            const parseOffers: JobOffer[] = onlyOffers.map( elementArchor => {
+                const children = Array.from( elementArchor!.children );
+    
+                const url = elementArchor?.getAttribute("href");      // obtiene el url de la oferta
+                const companyName = children.map( child => child.querySelector("h3")?.textContent )[0];     // obtiene el nombre de la compania
+                const description = children.map( child => child.querySelector("p")?.textContent )[0];      // obtiene la descripcion basica del trabajo
+    
+                // OBTENER LA LOCACION DEL TRABAJO
+                const pinIconElement = children.map( child => child.querySelector(`[name=icon-light-location-pin]`) )[0]    // obtener primero el icono pin
+                const location = pinIconElement?.nextElementSibling?.textContent         // obtener el elemento hermano del icono, el cual es un H3 con la localizacion y obtener el texto
+    
+                // OBTENER LA MODALIDAD DEL TRABAJO
+                const officeIconElement = children.map( child => child.querySelector(`[name=icon-light-office]`) )[0]    // obtener primero el icono de la oficina
+                const modality = officeIconElement?.nextElementSibling?.textContent         // obtener el elemento hermano del icono, el cual es un H3 con la modalidad y obtener el texto
+    
+                return {
+                    url: url || null,
+                    companyName: companyName || null,
+                    description: description || null,
+                    location: location || null,
+                    modality: modality || null
+                }
+            })
+    
+            return parseOffers;
+        })
+
+        worksFounds.push( ...offersWorksPage );
+        allWorks.push( ...offersWorksPage );
+
+
+
+    } while ( worksFounds.length !== 0 );
+
+    writeFileSync("data.json", JSON.stringify( allWorks ), "utf-8")
+
+    console.log("DONE")
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+    // await page.waitForSelector("#listado-avisos")           // espera a que la pagina y las ofertas de carguen
+
+    // const getOffersWork: JobOffer[] = await page.evaluate( () => {
+    //     const div = document.querySelector("#listado-avisos");  // esta es la seccion que contienete todos las ofertas de trabajo
+    //     const children = Array.from(div!.children);             // obtiene todos los hijos de la seccion anterior
+
+    //     const sectionOffers = children
+    //         .filter(( element => element.tagName === "DIV" ))   // obtiene unicamente los divs, que son las ofertas y una parte inutil
+    //         .map( element => element.firstElementChild );       // obtiene el elemento que contiene toda la informacion de la oferta
+
+    //     const onlyOffers = sectionOffers.slice (0, sectionOffers.length-1 ); // elimina la parte inutil que se encontraba al final del array
+
+
+    //     const parseOffers: JobOffer[] = onlyOffers.map( elementArchor => {
+    //         const children = Array.from( elementArchor!.children );
+
+    //         const url = elementArchor?.getAttribute("href");      // obtiene el url de la oferta
+    //         const companyName = children.map( child => child.querySelector("h3")?.textContent )[0];     // obtiene el nombre de la compania
+    //         const description = children.map( child => child.querySelector("p")?.textContent )[0];      // obtiene la descripcion basica del trabajo
+
+    //         // OBTENER LA LOCACION DEL TRABAJO
+    //         const pinIconElement = children.map( child => child.querySelector(`[name=icon-light-location-pin]`) )[0]    // obtener primero el icono pin
+    //         const location = pinIconElement?.nextElementSibling?.textContent         // obtener el elemento hermano del icono, el cual es un H3 con la localizacion y obtener el texto
+
+    //         // OBTENER LA MODALIDAD DEL TRABAJO
+    //         const officeIconElement = children.map( child => child.querySelector(`[name=icon-light-office]`) )[0]    // obtener primero el icono de la oficina
+    //         const modality = officeIconElement?.nextElementSibling?.textContent         // obtener el elemento hermano del icono, el cual es un H3 con la modalidad y obtener el texto
+
+    //         return {
+    //             url,
+    //             companyName,
+    //             description,
+    //             location,
+    //             modality
+    //         }
+    //     })
+
+    //     return parseOffers;
+    // })
+    
+    // console.log( getOffersWork )
+    // console.log( root )
+    // writeFileSync( "test.html", JSON.stringify( root ), "utf-8" )
+    // writeFileSync( "test.json", JSON.stringify( root ), "utf-8" )
---
+++ server/types/jobOffer.ts
@@ -0,0 +1,7 @@
+export interface JobOffer {
+    url:          string | null;
+    companyName:  string | null;
+    description:  string | null;
+    location:     string | null;
+    modality:     string | null;
+}
