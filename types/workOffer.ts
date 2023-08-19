14432c8c5239ef2562a8271d085611594224cec8
Juan Meza <cito2310@gmail.com>
2023-08-19 20:55:10 -0300

    changes types

--- types/workOffer.ts
+++ types/workOffer.ts
@@ -1,10 +1,10 @@
 export interface WorkOffer {
-    url:          string | null;
+    url:          string;
     companyName:  string | null;
     description:  string | null;
     location:     string | null;
     modality:     string | null;
-    title:        string | null;
+    title:        string;
 }
 
 export interface WorkOfferWithStatus extends WorkOffer {
